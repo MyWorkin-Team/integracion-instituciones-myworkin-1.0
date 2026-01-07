332c869823f6d0d643dd86d645e34b079b5a9f02
Piero <varaspiero19@gmail.com>
2026-01-07 10:06:16 -0500

    update

--- app/application/student/update_student_use_case.py
+++ app/application/student/update_student_use_case.py
@@ -1,7 +1,6 @@
 from datetime import datetime, timezone
 from app.domain.model.student import Student
 from app.domain.port.student_repository_port import StudentRepositoryPort
-from app.infrastructure.firebase.firebase_client import update_firebase_user
 
 class UpdateStudentByCoIdPsUseCase:
 
@@ -9,13 +8,6 @@ class UpdateStudentByCoIdPsUseCase:
         self.repo = repo
 
     def execute(self, co_id_ps: str, student: Student) -> bool:
-
-        # # 🔥 1️⃣ Actualizar Firebase (igual que en Register)
-        # update_firebase_user(
-        #     uid=student.firebase_uid,
-        #     email=student.email,
-        #     display_name=student.displayName,
-        # )
 
         # 🔥 2️⃣ Actualizar Firestore
         data = student.to_firestore_dict()
